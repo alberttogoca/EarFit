4ca57a4dd921497d75e009dc495e93daa38422ac
alberttogoca <alberttogoca@gmail.com>
2021-03-25 20:41:38 +0100

    Add meta tag description

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,10 @@ export default function Home(): JSX.Element {
       <Head>
         <title>EarFit</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta
+          name="description"
+          content="EARFIT: Aplicación para entrenamiento auditivo musical basada en Next.js y Typescript"
+        ></meta>
       </Head>
 
       {/*Container*/}
