282d6ddb343e0547cb97cedca67e8e7542cd7302
alberttogoca <alberttogoca@gmail.com>
2021-03-26 01:30:28 +0100

    remove api

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Request, Response } from 'express';
-
-export default function handler(req: Request, res: Response): void {
-  if (req.method === 'GET') {
-    res.status(200).json({ name: 'John Doe' });
-  }
-}
