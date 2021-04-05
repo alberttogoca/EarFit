1b986dd6847329d2e80f47111b31e827bdda8ba7
alberttogoca <alberttogoca@gmail.com>
2021-04-05 20:14:52 +0200

    Add intervals preview

--- pages/intervals/index.tsx
+++ pages/intervals/index.tsx
@@ -7,33 +7,35 @@ import PianoBasic from 'components/PianoBasic';
 import { useInstrumentContext } from 'context/SoundfontContext';
 import React, { useEffect, useState } from 'react';
 import { getRandomItem } from 'utils/arrayUtils';
+
+interface IInterval {
+  note1: string;
+  note2: string;
+}
+
 export default function Intervals(): JSX.Element {
   const { instrument } = useInstrumentContext();
-  const [intervals, setIntervals] = useState<(Interval | NoInterval)[]>([]);
-  const [answer, setAnswer] = useState<Interval | NoInterval>(undefined);
+  const [intervals, setIntervals] = useState<string[]>([]);
+  const [answer, setAnswer] = useState<IInterval>(undefined);
 
   useEffect(() => {
     console.log(IntervalDict.names());
-    const p1 = IntervalDict.get('1P');
-    const m2 = IntervalDict.get('2M');
-    const m3 = IntervalDict.get('3M');
-
-    const intervalArray = [p1, m2, m3];
-    setIntervals(intervalArray);
-    setAnswer(getRandomItem(intervalArray));
+    const answer = { note1: 'C4', note2: 'G4' };
+    setIntervals(IntervalDict.names());
+    setAnswer(answer);
   }, []);
 
   async function playInterval(): Promise<void> {
     instrument?.stop();
     //TO DO: sacar de answer.notes este array
     const intervalToPlay = [
-      { note: 'C3', time: 0 },
-      { note: 'Bb3', time: 0.5 },
+      { note: 'C4', time: 0 },
+      { note: 'G4', time: 0.5 },
     ];
 
     instrument?.schedule(0, intervalToPlay);
 
-    console.log(`Answer: ${answer.name}`);
+    console.log(`Answer: ${IntervalDict.distance(answer.note1, answer.note2)}`);
   }
 
   function handlePlay(): void {
@@ -41,11 +43,12 @@ export default function Intervals(): JSX.Element {
   }
 
   async function handleOption(option: string): Promise<void> {
-    console.log(option === answer.name);
-    if (option === answer.name) {
-      const newinterval = getRandomItem(intervals);
-      setAnswer(newinterval);
-      console.log(`New Interval: ${newinterval.name}`);
+    const result = IntervalDict.distance(answer.note1, answer.note2);
+    console.log(option === result);
+    if (option === result) {
+      //const newinterval = getRandomItem(intervals);
+      //setAnswer(newinterval);
+      //console.log(`New Interval: ${newinterval.name}`);
       //TO DO: Asegurarse de que el answer se ha actualizado antes de playScale
       playInterval();
     }
@@ -73,13 +76,8 @@ export default function Intervals(): JSX.Element {
         <div className="btn-group btn-group-toggle d-flex justify-content-center" data-toggle="buttons">
           <div>
             {intervals.map((interval) => (
-              <button
-                key={interval.name}
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => handleOption(interval.name)}
-              >
-                {interval.name.toUpperCase()}
+              <button key={interval} type="button" className="btn btn-secondary" onClick={() => handleOption(interval)}>
+                {interval.toUpperCase()}
               </button>
             ))}
           </div>
