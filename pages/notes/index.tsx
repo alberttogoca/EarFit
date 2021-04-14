797a786719d4a6e4f664486ae04ab9a0b5db0108
alberttogoca <alberttogoca@gmail.com>
2021-04-14 13:04:47 +0200

    Add red buttons to notes (need fix)

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -11,6 +11,9 @@ export default function Notes(): JSX.Element {
   const { instrument } = useInstrumentContext();
   const [notes, setNotes] = useState<string[]>([]);
   const [answer, setAnswer] = useState<string>();
+  //red buttons:
+  const [enable, setEnable] = useState<boolean>(true);
+  const optionClassName = enable ? 'btn btn-secondary' : 'btn btn-danger';
 
   useEffect(() => {
     const notes = Scale.get('C3 major').notes;
@@ -28,11 +31,14 @@ export default function Notes(): JSX.Element {
   function handleOption(option: string): void {
     console.log(option === answer);
     if (option === answer) {
+      //instrument?.stop(); //Replace this
+      setEnable(true); //red buttons
       const note = getRandomItem(notes);
       setAnswer(note);
-      //instrument?.stop(); //Replace this
       instrument?.play(note, 0, { duration: 2 });
       console.log(`Now playing: ${note}`);
+    } else {
+      setEnable(!enable); //red buttons
     }
   }
 
@@ -57,7 +63,7 @@ export default function Notes(): JSX.Element {
         {/*OPCIONES*/}
         <div className="btn-group btn-group-toggle d-flex justify-content-center" data-toggle="buttons">
           {notes.map((note) => (
-            <button key={note} type="button" className="btn btn-secondary" onClick={() => handleOption(note)}>
+            <button key={note} type="button" className={optionClassName} onClick={() => handleOption(note)}>
               {note[0]}
             </button>
           ))}
