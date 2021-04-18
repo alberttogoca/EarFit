d0d7b99b9a0a033f841e0e5620f98b3236cabb7f
alberttogoca <alberttogoca@gmail.com>
2021-04-18 18:05:24 +0200

    Add calculating allPosibleIntervals and intervalSet for options

--- src/pages/intervals/index.tsx
+++ src/pages/intervals/index.tsx
@@ -28,28 +28,34 @@ export default function Intervals(): JSX.Element {
   const optionClassName = enable ? 'btn btn-secondary' : 'btn btn-danger';
 
   useEffect(() => {
-    console.log(Interval.simplify(Interval.distance('B#3', 'Eb')));
     const tonic = 'C';
     const octave = '3';
     const pattern = 'major';
     const modes = Scale.modeNames(tonic + octave + ' ' + pattern);
-    const scaleList = modes.map(([r, n]) => Scale.get([r, n]));
+    const scaleList = modes.map(([root, name]) => Scale.get([root, name]));
     const noteList = scaleList[0].notes; //major
-    //noteList.forEach((n) => console.log(Interval.distance('B3', n)));
     setNotes(noteList);
     //INFO: +/-: direccion, 2: distancia notas, P/M/m/A/dd/d?: justa/mayor/menor/aumentado/disminuido/? (semitonos)
-    setOptions(Interval.names()); // TO DO: sacar todos los intervalos de las notas que se van a tocar
+    //const almostAllIntervals = ['1P', '2m', '2M', '3m', '3M', '4P', '4A', '5P', '6m', '6M', '7m', '7M'];
+
+    const intervalsSet = new Set<string>();
+    const allPosbileIntervals = noteList.map(
+      (firstnote) => noteList.map((secondnote) => Interval.distance(firstnote, secondnote).slice(-2)) //slice es para quitar la direccion
+    );
+    allPosbileIntervals.forEach((intervalList) => intervalList.forEach((item) => intervalsSet.add(item)));
+    const intervals = Array.from(intervalsSet).sort();
+    setOptions(intervals);
+
     const n1 = getRandomItem(noteList);
-    const n2 = getRandomItem(noteList); // TO DO: que esta nota sea siempre mayor
+    const n2 = getRandomItem(noteList); // TO DO: ¿que esta nota sea siempre mayor?
     const value = { note1: n1, note2: n2 };
-    const name = Interval.distance(value.note1, value.note2);
+    const name = Interval.distance(value.note1, value.note2).slice(-2);
     const answer = { name, value };
     setAnswer(answer);
   }, []);
 
   async function playAnswer(answer: Answer): Promise<void> {
-    //instrument?.stop(); //Replace thiss
-    //TO DO: sacar de answer.notes este array
+    //instrument?.stop(); //Replace this
     const intervalToPlay = [
       { note: answer.value.note1, time: 0, duration: 2 },
       { note: answer.value.note2, time: 0.5, duration: 2 },
@@ -72,7 +78,7 @@ export default function Intervals(): JSX.Element {
       const n1 = getRandomItem(notes);
       const n2 = getRandomItem(notes);
       const value = { note1: n1, note2: n2 };
-      const name = Interval.distance(value.note1, value.note2);
+      const name = Interval.distance(value.note1, value.note2).slice(-2);
       const newAnswer = { name, value };
       setAnswer(newAnswer);
       playAnswer(newAnswer);
