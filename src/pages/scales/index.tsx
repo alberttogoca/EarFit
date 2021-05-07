e2089f38fb08968df45108220db77bdf683d97d2
alberttogoca <alberttogoca@gmail.com>
2021-05-07 14:29:04 +0200

    Add useScales

--- src/pages/scales/index.tsx
+++ src/pages/scales/index.tsx
@@ -1,12 +1,12 @@
 import { Scale as ScaleType } from '@tonaljs/scale';
-import { Scale } from '@tonaljs/tonal';
+//import { Scale } from '@tonaljs/tonal';
 import { Configuration, Options, PlayButton, Title } from 'components/Exercise';
 import ExerciseLayout from 'components/Layout/ExerciseLayout';
 import { Menu } from 'components/Menu';
 import { Piano } from 'components/Piano';
 import { useInstrumentContext } from 'context/SoundfontContext';
-import React, { useEffect, useState } from 'react';
-import { getRandomItem } from 'utils/arrayUtils';
+import useScales from 'hooks/useScales';
+import React, { useState } from 'react';
 
 interface Answer {
   name: string;
@@ -15,27 +15,11 @@ interface Answer {
 
 export default function Scales(): JSX.Element {
   const { instrument } = useInstrumentContext();
-  const [scales, setScales] = useState<ScaleType[]>([]);
-  const [options, setOptions] = useState<string[]>([]);
-  const [answer, setAnswer] = useState<Answer>();
+  const { options, answer, setNewAnswer } = useScales();
+
   //red buttons:
   const [enable, setEnable] = useState<boolean>(true);
   const optionClassName = enable ? 'btn btn-secondary' : 'btn btn-danger';
-
-  useEffect(() => {
-    const tonic = 'C';
-    const octave = '3';
-    const pattern = 'major';
-    const modes = Scale.modeNames(tonic + octave + ' ' + pattern);
-    const scaleList = modes.map(([root, mode]) => Scale.get([root, mode])); //Obtaining notes for each mode
-    const scaleNames = scaleList.map((m) => m.type.toUpperCase());
-    setScales(scaleList);
-    setOptions(scaleNames);
-    const value = getRandomItem(scaleList);
-    const name = value.type;
-    const newAnswer = { name, value };
-    setAnswer(newAnswer);
-  }, []);
 
   function playAnswer(answer: Answer): void {
     //instrument?.stop(); //Replace this
@@ -53,10 +37,7 @@ export default function Scales(): JSX.Element {
   function handleOption(option: string): void {
     if (option.toUpperCase() === answer.name.toUpperCase()) {
       setEnable(true); //red buttons
-      const value = getRandomItem(scales);
-      const name = value.type;
-      const newAnswer = { name, value };
-      setAnswer(newAnswer);
+      const newAnswer = setNewAnswer();
       playAnswer(newAnswer);
     } else {
       setEnable(!enable); //red buttons
