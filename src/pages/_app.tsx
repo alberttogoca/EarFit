e562ebed34c9e27bf1e09568ece38aa47f2ed462
alberttogoca <alberttogoca@gmail.com>
2021-06-17 17:03:20 +0200

    add bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import 'bootswatch/dist/pulse/bootstrap.min.css';
+import 'bootswatch/dist/pulse/bootstrap.min.css'; //tenia la 4.5 y he actualizado a 5.0 -> me ha quitado los instrumentos de abajo y agrandado la letra
+//import 'bootstrap/dist/css/bootstrap.css' //con bootstrap normal sigue sin ir el desplegable
 import 'styles/global.css';
 
 import Layout from 'components/Layout';
