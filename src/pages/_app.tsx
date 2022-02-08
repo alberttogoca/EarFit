8b6464041fd372b5e3ae6a75ca19666afd7afb71
alberttogoca <alberttogoca@gmail.com>
2022-02-08 21:21:16 +0100

    make PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,13 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <Head>
         <title>EarFit</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/ico/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/ico/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/ico/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="194x194" href="/ico/apple-icon-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="194x194" href="/ico/apple-icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
