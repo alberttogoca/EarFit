61fa74352687cf53493ef74ce0b228f8e9d65bca
alberttogoca <alberttogoca@gmail.com>
2022-03-08 13:57:11 +0100

    Fix a tag

--- src/components/Layout/Header/MenuButton/index.tsx
+++ src/components/Layout/Header/MenuButton/index.tsx
@@ -6,14 +6,16 @@ export const MenuButton = (): JSX.Element => {
   const { pathname } = useRouter();
   const isHome = pathname === '/';
   return (
-    <Link href="/">
-      <a>
-        {!isHome && (
-          <Button variant={'link'} size="sm">
-            <h3>=</h3>
-          </Button>
-        )}
-      </a>
-    </Link>
+    <>
+      {!isHome && (
+        <Link href="/">
+          <a>
+            <Button variant={'link'} size="sm">
+              <h3>=</h3>
+            </Button>
+          </a>
+        </Link>
+      )}
+    </>
   );
 };
